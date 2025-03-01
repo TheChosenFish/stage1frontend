b038b0be7ce1bdc36088a5f21fd28d35ac39a188
Michael B <mbostick94@yahoo.com>
2025-03-01 07:39:48 -0500

    add stage 1.1 components

---
+++ stage1frontend/src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header></Header>
+      <Routes></Routes>
+    </>
+  );
+}
+
+export default App;
